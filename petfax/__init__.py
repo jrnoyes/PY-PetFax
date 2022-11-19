074f32bc7474ed4705be08927ce7d545ef72a271
Jason Noyes <jrnoyes1@gmail.com>
2022-11-19 14:55:49 -0600

    assignment 5

---
+++ petfax/__init__.py
@@ -0,0 +1,32 @@
+# config                    
+from flask import Flask
+
+# factory
+def create_app():
+    app = Flask(__name__)
+
+    # index route
+    @app.route('/')
+    def index(): 
+        return 'Hello, PetFax!'
+
+    @app.route('/pets')
+    def pets():
+        return "this is the new pets route"
+    
+    @app.route('/facts')
+    def facts():
+        return "this is the facts page"
+
+    # register pet blueprint 
+    from . import pet 
+    app.register_blueprint(pet.bp)
+
+    # register fact blueprint 
+    from . import fact
+    app.register_blueprint(fact.bp)
+
+    # return the app 
+    return app
+
+
